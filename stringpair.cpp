6dd2ead696cdeda85406a62c7f85b7c029d0a53e
Mandeepkumarsaw <mandeepkr9386@gmail.com>
2025-05-10 00:47:14 +0530

    Added Flexibility

--- stringpair.cpp
+++ stringpair.cpp
@@ -6,24 +6,50 @@
 
 using namespace std;
 
-class stringpair{
-    public:
-    int maximumNumberOfStringPairs(vector<string>& words){
-        int tally=0;
-        unordered_set<string> seen;
+//using reverse & loop
+class stringpair {
+public:
+    int maximumNumberOfStringPairs(std::vector<std::string>& words) {
+        int n = words.size();
+        int tally= 0;
 
-        for(auto &s:words){
-            string reversed =s;
-            reverse(reversed.begin(),reversed.end());
-
-            if(seen.count(reversed)){
-                tally++;
-            }
-            seen.insert(s);
+        vector<string> reversedWords = words; // Preserve original words
+        for (auto &s : reversedWords) {
+            reverse(s.begin(), s.end()); // Reverse each word
         }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {        //O(n^2*k)  O(N)
+                if(reversedWords[i] == words[j]) {
+                    tally++;
+                }
+            }
+        }
+
         return tally;
     }
 };
+
+
+//using unordered set
+// class stringpair{
+//     public:
+//     int maximumNumberOfStringPairs(vector<string>& words){
+//         int tally=0;
+//         unordered_set<string> seen;
+
+//         for(auto &s:words){
+//             string reversed =s;               //O(n*k),k= strings O(N)
+//             reverse(reversed.begin(),reversed.end());
+
+//             if(seen.count(reversed)){
+//                 tally++;
+//             }
+//             seen.insert(s);
+//         }
+//         return tally;
+//     }
+// };
 
 int main(){
     vector<string> words = {"cd","ac","dc","ca","zz"};
