c4048fe3d287833c4d8af239920eae38d3abe220
Mandeepkumarsaw <mandeepkr9386@gmail.com>
2025-05-24 23:52:54 +0530

    Lp:647 Palindrome substring count

---
+++ plaindromestr.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class palindromestr {
+public:
+    int countSubstrings(string s) {
+        int res=0;
+        
+        for(int i=0;i<s.size();i++){
+           int l=i;
+           int r=i;
+                  while(l>=0 && r<=s.size() && s[l]==s[r]){
+                     res +=1;
+                     l--;
+                     r++;
+                    }
+            l=i;
+            r=i+1;
+                   while(l>=0 && r<=s.size() && s[l]==s[r]){
+                     res +=1;
+                     l--;
+                     r++;
+                    }
+        }
+      return res;
+    }
+};
+
+int main(){
+    string s = "aba";
+    palindromestr obj;
+    int res=obj.countSubstrings(s);
+
+    cout<<"Count is:"<<res<<endl;
+    return 0;
+}
