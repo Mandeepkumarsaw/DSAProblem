ad50c08f64f078e6b30490f2f4f1344f3155192c
Mandeepkumarsaw <mandeepkr9386@gmail.com>
2025-05-30 23:54:10 +0530

    Lp:746 Min climb Most Stairs

---
+++ mincostclimb.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>    
+using namespace std;
+
+class mincostclimb {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int> minCost(n+1,0);     // Use vector for dynamic size
+
+
+        for(int i=2;i<=n;i++){
+            minCost[i]=
+            min(
+                (cost[i-1]+minCost[i-1]),
+                (cost[i-2]+minCost[i-2])
+                );
+
+            }
+            return minCost[n];
+        }
+};
+
+int main(){
+    vector<int> cost={10,15,20};
+    mincostclimb obj;
+    int res=obj.minCostClimbingStairs(cost);
+    cout<<"The minimum cost of climbing stairs: "<<res<<endl;
+
+    
+}
