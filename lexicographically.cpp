59e4d5a169943e181f66a7136857126f179ca2c6
Mandeepkumarsaw <mandeepkr9386@gmail.com>
2025-06-07 18:00:13 +0530

    LP3170 Lexicographically minimum string after removing star(*).

---
+++ lexicographically.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>    
+using namespace std;
+
+class lexicographically {
+public:
+    typedef pair<char,int> P;
+
+    struct comp{
+        bool operator()(P &p1,P &p2){
+              if(p1.first == p2.first){
+                  return p1.second < p2.second;
+          }
+
+        return p1.first > p2.first;
+       }
+    };
+
+    string clearStars(string s) {
+        int n=s.length();
+
+        priority_queue<P,vector<P>,comp> pq;
+
+        for(int i=0;i<n;i++){
+            if(s[i] != '*'){
+                pq.push({s[i],i});
+            } else {
+                int idx = pq.top().second;
+                pq.pop();
+                s[idx] = '*';
+            }
+        }
+        string result ="";
+        for(int i=0;i<n;i++){
+            if(s[i] != '*'){
+                result.push_back(s[i]);
+            }
+        }
+        return result;
+    }
+};
+
+int main(){
+    string s = "abcsdg*cxfd*";
+    lexicographically obj;
+    string res=obj.clearStars(s);
+
+    for(const& num:res){
+       cout<<"Lexicographically: "<<res<<endl;
+    }
+
+
+}
