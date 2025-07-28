46f1436df978bfa7f2f69613eb06dd53358747be
Mandeepkumarsaw <mandeepkr9386@gmail.com>
2025-07-28 22:51:05 +0530

    Lp:78 Subsets with recursion(duplicates)

---
+++ Subset.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>    
+using namespace std;
+
+class Subset {
+public:
+    void getAllSubsets(vector<int>& nums, vector<int> &ans, int i,   //helper function
+                       vector<vector<int>> &allSubsets) {
+
+        if (i == nums.size()) {                 //base case
+            allSubsets.push_back({ans});
+            return;
+        }
+
+        // include
+        ans.push_back(nums[i]);
+        getAllSubsets(nums, ans, i + 1, allSubsets);
+
+        ans.pop_back();                   //backtracking
+
+        // exclude
+        getAllSubsets(nums, ans, i + 1, allSubsets);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {                                //main func
+        vector<vector<int>> allSubsets;                         //stores the final result
+        vector<int> ans;                                                     //is the current subset being built.
+
+        getAllSubsets(nums, ans, 0, allSubsets);          // Start Recursion
+
+
+        return allSubsets;                                                  //After recursion completes, return all subsets
+    }
+};
+
+
+int main() {
+    Subset sol;
+    vector<int> nums = {1, 2, 3};  
+
+    vector<vector<int>> result = sol.subsets(nums);
+
+    cout << "All subsets:\n";
+    for (const auto& subset : result) {
+        cout << "[";
+        for (int num : subset) {
+            cout << num << "";
+        }
+        cout << "]\n";
+    }
+
+    return 0;
+}
