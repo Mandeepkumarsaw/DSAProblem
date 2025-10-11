4e7282b7e7552aa6aac42e8746ac8629ebccf1c6
Mandeepkumarsaw <mandeepkr9386@gmail.com>
2025-10-11 21:02:09 +0530

    Lp:42 Trapping Rain Water_O(n)

---
+++ trappingrainwater.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l=0 , r=n-1;
+        int lmax = 0, rmax = 0;
+        int ans = 0;
+
+        while(l < r) {
+            lmax = max(lmax,height[l]);
+            rmax = max(rmax,height[r]);
+
+            if(lmax < rmax) {
+               ans += lmax - height[l];
+               l++;
+            } else {
+               ans += rmax - height[r];
+               r--;
+            }
+        } 
+        return ans;       
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> heights = {0,1,0,2,1,0,1,3,2,1,2,1};
+
+    int ans = obj.trap(heights);
+    cout<<"Maximum water holding capacity: "<<ans;
+
+    return 0;
+}
